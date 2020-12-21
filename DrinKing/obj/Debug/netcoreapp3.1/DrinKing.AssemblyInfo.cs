41112bbb9de2af7e19c667e313984250f25136ef
Şeyda Gündoğdu <seyda.gundogdu@ogr.sakarya.edu.tr>
2020-12-21 17:21:56 +0300

    update

--- DrinKing/obj/Debug/netcoreapp3.1/DrinKing.AssemblyInfo.cs
+++ DrinKing/obj/Debug/netcoreapp3.1/DrinKing.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-DrinKing-178FD516-FB86-4EC1-BFC4-3F75C5BA351A")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrinKing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
