5c0f726ff6032e5363da0b512a5d350925907f3e
SANEM KOCA <Sanemkoca@users.noreply.github.com>
2021-01-07 23:05:23 +0300

    ....

---
+++ DrinKing/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using DrinKing.Data.Interfaces;
+using DrinKing.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinKing.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
--- DrinKing/obj/Release/netcoreapp3.1/DrinKing.AssemblyInfo.cs
+++ DrinKing/obj/Release/netcoreapp3.1/DrinKing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-DrinKing-178FD516-FB86-4EC1-BFC4-3F75C5BA351A")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrinKing")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrinKing")]
